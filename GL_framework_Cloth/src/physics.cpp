bb9bcbaeff7c03d940c83303206d993ea0dbdbae
Eloi Salles Torres <Eloi Salles Torres>
2017-04-09 17:49:18 +0200

    Fa col·lisio amb l'esfera i esta posada la gui

--- GL_framework_Cloth/src/physics.cpp
+++ GL_framework_Cloth/src/physics.cpp
@@ -19,7 +19,7 @@ namespace ClothMesh
 
 namespace Sphere
 {
-	extern void updateSphere(glm::vec3 pos, float radius = 1.f);
+	extern void updateSphere(glm::vec3 pos, float radius);
 }
 
 struct Particle
@@ -34,7 +34,14 @@ struct ParticlesList
 	std::vector<Particle> list;
 	float elastic = 0.5f;
 	float time;
-	float damping=0.5f;
+	float dampingStretch = 0.5f;
+	float dampingShear = 0.5f;
+	float dampingBend = 0.5f;
+	float longitud = 10;
+	float initalDistancePoints = 0.5;
+	glm::vec3 centerSphere;
+	float r = 1.0f;
+
 	float* ParticlesToFloatPointer()
 	{
 		float* v= new float[list.size()*3];
@@ -55,11 +62,11 @@ struct ParticlesList
 
 		for (int j = 0; j < ClothMesh::numRows; j++)
 		{
-			z += 0.5f;
+			z += initalDistancePoints;
 			x = -4.5f;
 			for (int i = 0; i < ClothMesh::numCols; i++)
 			{
-				x += 0.5;
+				x += initalDistancePoints;
 				Particle p(x, y, z);
 				p.xV = 0;
 				p.yV = 0;
@@ -70,20 +77,19 @@ struct ParticlesList
 				list.push_back(p);
 			}
 		}
-		float r = (rand() % 1) + 1;
-		glm::vec3 s;
-		s.x = (rand() % 10 - r) - 5;
-		s.y = rand() % 5;
-		s.z = (rand() % 10 - r) - 5;
-		std::cout << s.x << "	" << s.y << "	" << s.z << std::endl;
-		Sphere::updateSphere(s, r);
+		r = (rand() % 1) + 1;
+		centerSphere.x = 0;//(rand() % 10 - r) - 5;
+		centerSphere.y = rand() % 5;
+		centerSphere.z = 0;//(rand() % 10 - r) - 5;
+		Sphere::updateSphere(centerSphere, r);
 
 		ClothMesh::updateClothMesh(ParticlesToFloatPointer());
 	}
+
 	float Spring(float p1, float p2,float v1, float v2,float L)
 	{
 		//F1 = -(Ke (||P1-P2|| - L12) + Kd (v1-v2) * (P1-P2)/(||P1-P2||) ) * (P1-P2)/(||P1-P2||)
-		float resultat = -(elastic*(abs(p1 - p2) - L)+damping*(v1-v2)*((p1 - p2) / abs(p1 - p2)))*((p1 - p2) / abs(p1 - p2));
+		float resultat = -(elastic*(abs(p1 - p2) - L)+dampingStretch*(v1-v2)*((p1 - p2) / abs(p1 - p2)))*((p1 - p2) / abs(p1 - p2));
 
 		return resultat;
 	}
@@ -97,8 +103,18 @@ struct ParticlesList
 			//Si es el primer.
 			if (i == 0)
 			{
+				//Horizontal
 				l= list[i].x - list[i + 1].x;
 				v.x = Spring(list[i].x, list[i + 1].x, list[i].xV, list[i + 1].xV, l);
+
+				l = list[i].y - list[i + 1].y;
+				v.y = Spring(list[i].y, list[i + 1].y, list[i].yV, list[i + 1].yV, l);
+
+				l = list[i].z - list[i + 1].z;
+				v.z = Spring(list[i].z, list[i + 1].z, list[i].zV, list[i + 1].zV, l);
+
+				//l = list[i].x - list[i + ClothMesh::numCols].x;
+				//v.x = Spring(list[i].x, list[i + ClothMesh::numCols].x, list[i].xV, list[i + ClothMesh::numCols].xV, l);
 			}
 			//Si es l'ultim de la fila.
 			else if (i + 1 == ClothMesh::numCols)
@@ -147,6 +163,19 @@ struct ParticlesList
 
 		return v;
 	}
+	bool DistancePointSphere(int i)
+	{
+		glm::vec3 v (list[i].x - centerSphere.x, list[i].y - centerSphere.y, list[i].z - centerSphere.z);
+		float d = glm::pow(v.x, 2) + glm::pow(v.y, 2) + glm::pow(v.z, 2);
+		d = glm::sqrt(d);
+
+		float x = list[i].x - centerSphere.x;
+		float y = list[i].y - centerSphere.y;
+		float z = list[i].z - centerSphere.z;
+
+		return d-r<= 0;
+	}
+
 	//Extret de la practica de rebot de les particules
 	void Update(float dt)
 	{
@@ -296,7 +325,17 @@ struct ParticlesList
 			CollisionParticlePlane(xAnterior, yAnterior, zAnterior, i, normalPla, dEquacioPla);
 		}
 
-
+		//ESFERA	
+		if (DistancePointSphere(i))
+		{
+			glm::vec3 vectorDirector(list[i].x - centerSphere.x, list[i].y - centerSphere.y, list[i].z - centerSphere.z);
+			vectorDirector = glm::normalize(vectorDirector);
+			glm::vec3 point = centerSphere + (vectorDirector*r);
+			
+			list[i].x = xAnterior;
+			list[i].y = yAnterior;
+			list[i].z = zAnterior;
+		}
 	}
 	void CollisionParticlePlane(float xAnterior, float yAnterior, float zAnterior, int i, float *normalPla, float dEquacioPla)
 	{
@@ -329,8 +368,18 @@ ParticlesList particles;
 void GUI() {
 	{	//FrameRate
 		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-
+		
 		//TODO
+		ImGui::Text("Using SemiImplicit Euler System");
+		ImGui::DragFloat("Stretch", &particles.dampingStretch, 0.1f,0, 30, "%.3f", 1.f);
+		ImGui::DragFloat("Shear", &particles.dampingShear, 0.1f, 0, 30, "%.3f", 1.f);
+		ImGui::DragFloat("Bend", &particles.dampingBend, 0.1f, 0, 30, "%.3f", 1.f);
+		ImGui::DragFloat("Enllongation", &particles.longitud, 0.1f, 0, 30, "%.3f", 1.f);
+		ImGui::DragFloat("Initial Rest Distance", &particles.initalDistancePoints, 0.1f, 0.1, 30, "%.3f", 1.f);
+		if (ImGui::Button("Reset", ImVec2(50, 20)))
+		{
+			particles.ResetValues();
+		}
 	}
 
 	// ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
